067d804733668892745e593863f5c0a434cee731
v.loianich <vloyanich@outlook.com>
2024-08-17 01:15:16 +0300

    function checkProbabilityTheory(count)

--- js_homework4.js
+++ js_homework4.js
@@ -1,2 +1,32 @@
-js_homework4
+//js_homework4
+
+
+var  z=0
+var  k2=0
+var  k1=0
+var k=0
+var q=0
+
+function checkProbabilityTheory(count) {
+
+    console.log('кількість випадкових чисел:' + count);
+    k=count;
+    for (let i = 1; i <= k; i++) { 
+        z=(100 - 0.5 + Math.random() * (1000 - 100 + 1));
+        q=Math.round(z);
+    if (q % 2) {console.log('непарне: ' + q); 
+      ++k1;}
+   else {console.log('парне: ' + q); ++k2;}
+    
+}}
+
+const result = checkProbabilityTheory(5);
+
+
+console.log(k1 + " непарних");
+console.log(k2 + " парних");
+let x=k1*100/k
+console.log(x + "% - відсоток непарних");
+let y=k2*100/k
+console.log(y + "% - відсоток парних");
 
--- js_test_hm_doc.js
+++ js_test_hm_doc.js
@@ -13,7 +13,7 @@
 
 
 
-  function isEven(number) {
+/*   function isEven(number) {
     if (number % 2 === 0) {
     return true;
 } else {
@@ -23,4 +23,160 @@
  
  const number = 8
 ;
- console.log(`Число ${number} парне: ${isEven(number)}`);
+ console.log(`Число ${number} парне: ${isEven(number)}`); */ 
+
+
+
+
+
+/* var  z=0
+k2=0
+k1=0
+   function random(min, max, n) {
+  
+     for (let i = 1; i <= n; i++) { 
+        z=min + Math.random() * (max - min);
+        q=z.toFixed(0)
+   
+
+    if (q % 2) {console.log('непарне: ' + q); 
+      ++k1;}
+   else {console.log('парне: ' + q); ++k2;}
+   
+     }
+     return q;
+   }
+
+
+console.log(random(100, 1000.0000000000000000000000000000000000000000000000000000000001, 5));
+console.log(k1 + " непарних");
+console.log(k2 + " парних"); */
+
+
+/* function pow(x,y)  {
+    
+    var z=1;   
+    for (let i = 1; i <= y; i++) { 
+         
+         z=z*x
+          console.log(z);
+        }
+    console.log(`x = ${x}`);
+    console.log(`y = ${y}`);
+    return z; 
+      
+    } 
+const result = pow(2,3);
+console.log(`x в степені y = ${result}`) */
+
+/* var  z=0
+var  k2=0
+var  k1=0
+var k=0
+   function random(min, max, n) {
+  k=n
+  console.log('кількість випадкових чисел:' + k)
+     for (let i = 1; i <= n; i++) { 
+        z=min + Math.random() * (max - min);
+        q=z.toFixed(0)
+   
+
+    if (q % 2) {console.log('непарне: ' + q); 
+      ++k1;}
+   else {console.log('парне: ' + q); ++k2;}
+   
+     }
+     return q;
+   }
+
+console.log(random(100, 1000.0000000000000000000000000000000000000000000000000000000001, 6));
+console.log(k1 + " непарних");
+console.log(k2 + " парних");
+let x=k1*100/k
+console.log(x + "% - відсоток непарних");
+let y=k2*100/k
+console.log(y + "% - відсоток парних"); */
+
+
+
+
+/* 
+
+
+var  z=0
+var  k2=0
+var  k1=0
+var k=0
+var q=0
+
+function checkProbabilityTheory(count) {
+
+    console.log('кількість випадкових чисел:' + count);
+    k=count;
+    for (let i = 1; i <= k; i++) { 
+        z=100 + Math.random() * (1000.0000000000000000000000000000000000000000000000000000000001 - 100);
+       // q=z.toFixed(0);
+       q=
+    if (q % 2) {console.log('непарне: ' + q); 
+      ++k1;}
+   else {console.log('парне: ' + q); ++k2;}
+    
+}}
+
+const result = checkProbabilityTheory(5);
+
+
+console.log(k1 + " непарних");
+console.log(k2 + " парних");
+let x=k1*100/k
+console.log(x + "% - відсоток непарних");
+let y=k2*100/k
+console.log(y + "% - відсоток парних");  */
+
+/* function randomInteger(min, max) {
+    // тепер rand від (min - 0.5) до (max + 0.5)
+    let rand = min - 0.5 + Math.random() * (max - min + 1);
+    return Math.round(rand);
+}
+
+console.log(randomInteger(100, 1000)); */
+
+
+
+
+var  z=0
+var  k2=0
+var  k1=0
+var k=0
+var q=0
+
+
+function checkProbabilityTheory(count) {
+
+    console.log('кількість випадкових чисел:' + count);
+    k=count;
+}
+
+console.log()
+
+for (let i = 1; i <= k; i++) { 
+    function randomInteger(min=100, max=1000) {
+        let rand = min - 0.5 + Math.random() * (max - min + 1);
+     
+        return intRand=Math.round(rand);;
+    };
+        
+    if (randomInteger() % 2) {console.log('непарне: ' + intRand); 
+      ++k1;}
+   else {console.log('парне: ' + intRand); ++k2;}
+    
+}
+
+const result = checkProbabilityTheory(5);
+
+console.log(k1 + " непарних");
+console.log(k2 + " парних");
+let x=k1*100/k
+console.log(x + "% - відсоток непарних");
+let y=k2*100/k
+console.log(y + "% - відсоток парних");
