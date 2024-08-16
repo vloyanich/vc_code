4dc750907e319bffe7e97abac3d2dc9660f46bcd
v.loianich <vloyanich@outlook.com>
2024-08-16 17:19:30 +0300

    function pow()

--- js_homework3.js
+++ js_homework3.js
@@ -1,1 +1,28 @@
-//js_homework3
+//js_homework3 
+
+
+/* let x=2, y =4, z=1
+  for (let i = 1; i <= y; i++) { 
+    z=z*x
+    console.log(z);
+   }  */
+
+
+ function pow(x,y)  {
+    
+    var z=1;   
+    for (let i = 1; i <= y; i++) { 
+         
+         z=z*x
+          console.log(z);
+        }
+    console.log(`x = ${x}`);
+    console.log(`y = ${y}`);
+    return z; 
+      
+    } 
+const result = pow(2,3);
+console.log(`x в степені y = ${result}`)
+
+  
+
